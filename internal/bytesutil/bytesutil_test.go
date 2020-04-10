ee656bc301b70a2730db7c81fea2793a2cc8c311
Vikram Melkote <vikram@suki.ai>
2020-04-10 14:10:21 -0700

    fixed incorrect reference to mediocre-go-lib package

--- internal/bytesutil/bytesutil_test.go
+++ internal/bytesutil/bytesutil_test.go
@@ -7,7 +7,7 @@ import (
 	"io"
 	. "testing"
 
-	"github.com/vikram-suki/mediocre-go-lib/mrand"
+	"github.com/mediocregopher/mediocre-go-lib/mrand"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
